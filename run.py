d4b9cbad92c130dfb2f92012199e72b43e7f5aaa
Nathan Perrier <107982946+nathan-perrier23@users.noreply.github.com>
2023-05-15 01:55:31 +0000

    cleaned up Code

--- run.py
+++ run.py
@@ -10,8 +10,6 @@ from data_source import DataSource
 from urllib import request as request_url
 from honeybadger.contrib import FlaskHoneybadger
 
-#hbp_MYOQ2R1pf24h8CnVXiXCtQbYv8Bijm4yNgCZ
-
 db = DATA()
 ai = Main() 
 
@@ -21,7 +19,6 @@ app.config['HONEYBADGER_ENVIRONMENT'] = 'production'
 app.config['HONEYBADGER_API_KEY'] = 'hbp_MYOQ2R1pf24h8CnVXiXCtQbYv8Bijm4yNgCZ'
 app.config['HONEYBADGER_PARAMS_FILTERS'] = 'password, secret, credit-card'
 FlaskHoneybadger(app, report_exceptions=True)
-
 
 theme = 'css/dist/theme/blood.css'
 
@@ -56,6 +53,7 @@ def name():
                 except Exception as e: print(e)
             return render_template("your_names.html", theme=theme, keys=keys, names=names, descriptions=descriptions, origins=origins, genders=genders)
     except: print('no names found 1'); return not_acceptable(406,'Input a valid prompt -', 'Invalid Prompt')
+    
 @app.route("/Fact Page", methods=['GET', 'POST']) 
 def fact_page():
     if request.method == 'POST':
@@ -72,8 +70,7 @@ def name_finder_page():
     return render_template("name_finder.html", theme=theme, placeholder="e.g. likes, interests, favourite things...")
 
 @app.route("/about", methods=['GET', 'POST'])
-def about_page():
-    return render_template("about_page.html", theme=theme, img=ai.get_img(random.choice(['family', 'kids', 'baby', 'child'])))
+def about_page(): return render_template("about_page.html", theme=theme, img=ai.get_img(random.choice(['family', 'kids', 'baby', 'child'])))
 
 @app.route("/boy names", methods=['GET', 'POST'])
 def top_100_boy_page(gender='Boy'):
@@ -94,13 +91,11 @@ def top_100_girl_page(gender='Girl'):
     return render_template("top_100.html", theme=theme, gender=gender, action='top_100_girl_page', names=names, counts=counts)
 
 @app.route("/contact", methods=['GET', 'POST'])
-def contact_page():
-    return render_template("contact_page.html", theme=theme)
+def contact_page(): return render_template("contact_page.html", theme=theme)
 
 @app.route("/contact form", methods=['GET', 'POST'])
 def contact_form_page():
-    if request.method == 'POST':
-        return thanks() 
+    if request.method == 'POST': return thanks() 
     return render_template("contact-form.html", theme=theme, title="Contact Form")
 
 @app.route("/thank you", methods=['GET', 'POST']) 
@@ -111,40 +106,28 @@ def thanks():
             except Exception as e: print(e)
             return render_template("form1.html", theme=theme, template="template-form1-2.html", display1="none", display2="none", display3="none", display4="none", display5="none", display6="none", display7="none", display8="none", display9="none", display10="block",  display11="block", title="Thank You", header=('Thanks ' + request.form['name'] + ' For Contacting Us!'), s_header='We will get back to you shortly!')
         return not_acceptable(406,'Input a valid email -', 'Invalid Email')
-    except:
-        return bad_request(400)
-    
-@app.route("/notes")
-def notes():
-    return render_template("speaker-view.html")
+    except: return bad_request(400)
 
 @app.route("/demo")
-def demo():
-    return render_template("demo.html")
+def demo(): return render_template("demo.html")
 
 @app.errorhandler(400)                                                        
-def bad_request(e):
-    return render_template('form1.html', template="template-form1.html", colour1='#333333', colour2='#777777', colour3='#dddddd', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Bad Request", header='400 Error', s_header='request could not be complete, please return to previous page'), 400      
+def bad_request(e): return render_template('form1.html', template="template-form1.html", colour1='#333333', colour2='#777777', colour3='#dddddd', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Bad Request", header='400 Error', s_header='request could not be complete, please return to previous page'), 400      
 
 @app.errorhandler(404)                                                        
-def page_not_found(e):
-    return render_template('form1.html', template="template-form1.html", colour1='#333333', colour2='#777777', colour3='#dddddd', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Page Not Found", header='404 Error', s_header='page not found, please return to previous page'), 404  
+def page_not_found(e): return render_template('form1.html', template="template-form1.html", colour1='#333333', colour2='#777777', colour3='#dddddd', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Page Not Found", header='404 Error', s_header='page not found, please return to previous page'), 404  
 
 @app.errorhandler(500)                                                
-def internal_sever_error(e):
-   return render_template('form1.html', template="template-form1.html", colour1='#333333', colour2='#777777', colour3='#dddddd', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Sever Error", header='500 Error', s_header='internal sever error, please check local host connectivity'), 500  
+def internal_sever_error(e): return render_template('form1.html', template="template-form1.html", colour1='#333333', colour2='#777777', colour3='#dddddd', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Sever Error", header='500 Error', s_header='internal sever error, please check local host connectivity'), 500  
 
 @app.errorhandler(401) 
-def unauthorized(e):
-    return render_template('form1.html', template="template-form1.html", colour1='#FF0000', colour2='#800000', colour3='#CA3433', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Validation Error", header='401 Error', s_header='access not granted, please return to previous page'), 401
+def unauthorized(e): return render_template('form1.html', template="template-form1.html", colour1='#FF0000', colour2='#800000', colour3='#CA3433', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Validation Error", header='401 Error', s_header='access not granted, please return to previous page'), 401
 
 @app.errorhandler(406) 
-def not_acceptable(e, error='Validation error,', header='406 Error'):
-    return render_template('form1.html', template="template-form1.html", colour1='#FF0000', colour2='#800000', colour3='#CA3433', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Input Validation Error", header=header, s_header=error+" please return to previous page and try again"), 406
+def not_acceptable(e, error='Validation error,', header='406 Error'): return render_template('form1.html', template="template-form1.html", colour1='#FF0000', colour2='#800000', colour3='#CA3433', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Input Validation Error", header=header, s_header=error+" please return to previous page and try again"), 406
 
 @app.errorhandler(408)                                                
-def timed_out(e):
-   return render_template('form1.html', template="template-form1.html", colour1='#333333', colour2='#777777', colour3='#dddddd', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Request Timed Out", header='408 Error', s_header='request timed out, please return to previous page'), 408  
+def timed_out(e): return render_template('form1.html', template="template-form1.html", colour1='#333333', colour2='#777777', colour3='#dddddd', display1="none", display2="none", display3="none", display4="none", display5="block", display6="none", display7="none", display8="none", display9="none", display10="block", display11="none", title="Request Timed Out", header='408 Error', s_header='request timed out, please return to previous page'), 408  
 
 
 if __name__ == '__main__':
